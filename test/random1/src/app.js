5febe1d07cb225e431f83683478926dcec90b610
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2019-04-02 19:57:37 -0700

    experimenting with a random test

--- test/random1/src/app.js
+++ test/random1/src/app.js
@@ -1,24 +1,24 @@
 (function () {
-    'use strict';
+  'use strict';
 
 var words = 'Lorem ipsum do&shy;lor sit amet, conse&shy;ctetur adipi&shy;sicing elit, sed do eiu&shy;smod tempor inci&shy;di&shy;dunt ut la&shy;bore et dolore magna aliqua. Ut enim ad minim veni&shy;am, quis nostrud exe&shy;rcitation ulla&shy;mco labo&shy;ris nisi ut aliquip ex ea commo&shy;do con&shy;sequ&shy;at. Dui&shy;s aute irure dolor in repre&shy;hende&shy;rit in voluptate velit esse cillum dolore eu fugi&shy;at nulla pari&shy;atur. Exce&shy;pteur sint occaecat cupidatat non proi&shy;dent, sunt in culpa qui offi&shy;cia deserunt mollit anim id est laborum'.split(' ');
 
 function randomText () {
     var res = [];
     var wlen = words.length;
-    var index, i, ilen = 500 * Math.random();
-    res.push('<p>');
+    var index, i, ilen = 500 * Math.random() + 500;
+    res.push('<p>(');
     for (i = 0; i < ilen; i++) {
         index = Math.round(wlen * Math.random());
         res.push(words[index]);
     }
-    res.push('</p>');
+    res.push(')</p>');
     return res.join(' ');
 }
 
 function randomChapter () {
     var res = [];
-    var i, ilen = 20 * Math.random();
+    var i, ilen = 20 * Math.random() + 20;
     res.push('<div>');
     for (i = 0; i < ilen; i++) {
         res.push(randomText());
@@ -28,26 +28,41 @@ function randomChapter () {
 }
 
 function nwPrint () {
-    if (nw === undefined) {
-        return;
-    }
+  if (nw === undefined) {
+      return;
+  }
 
-    var fs = require('fs'),
-        path = require('path');
+  var fs = require('fs'),
+      path = require('path');
 
-    var dumpFileName = path.resolve(process.env.PWD, 'datasheet.pdf');
+  var dumpFileName = path.resolve(process.env.PWD, 'datasheet.pdf');
 
-    var win = nw.Window.get();
+  var win = nw.Window.get();
 
-    setTimeout(function () {
-        win.print({
-            pdf_path: path.resolve(dumpFileName),
-            headerFooterEnabled: false
-        });
-        setTimeout(function () {
-            process.exit(0);
-        }, 7000);
-    }, 3000);
+  setTimeout(function () {
+      win.print({
+          pdf_path: path.resolve(dumpFileName),
+          headerFooterEnabled: false,
+          shouldPrintBackgrounds: true,
+          marginsType: 3,
+          marginsCustom: {
+            marginBottom: 40,
+            marginLeft: 60,
+            marginRight: 60,
+            marginTop: 20
+          },
+          mediaSize: {
+            name: 'CUSTOM',
+            width_microns: 215900,
+            height_microns: 279400,
+            custom_display_name: 'Letter',
+            is_default: true
+          }
+      });
+      setTimeout(function () {
+          process.exit(0);
+      }, 7000);
+  }, 3000);
 }
 
 var contentDiv = document.getElementById('content');
@@ -57,3 +72,4 @@ contentDiv.innerHTML = randomChapter();
 nwPrint();
 
 })();
+/* eslint strict: 0, indent: 0 */
